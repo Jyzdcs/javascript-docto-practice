b06aa22a372a992543d52b232458afb45e3d7c83
Jyzdcs <ky.claudant@gmail.com>
2025-11-24 18:57:08 +0100

    Add hashmap_set.js with implementations of HashMap and HashSet classes
    - Implemented HashMap class with methods for setting, getting, checking existence, and deleting key-value pairs.
    - Implemented HashSet class with methods for adding values, checking existence, and deleting values.
    - Included commented-out example usages for testing the functionality of both classes.

---
+++ modules/02-data-structures/ex03-hashmap-set/hashmap_set.js
@@ -0,0 +1,73 @@
+class HashMap {
+  constructor() {
+    this.hashMap = {};
+  }
+
+  set(key, value) {
+    this.hashMap[key] = value;
+  }
+
+  get(key) {
+    return this.hashMap[key];
+  }
+
+  has(key) {
+    return this.hashMap[key] !== undefined;
+  }
+
+  delete(key) {
+    if (this.hashMap[key]) {
+      delete this.hashMap.key;
+      return true;
+    }
+    return false;
+  }
+}
+
+class HashSet {
+  constructor() {
+    this.hashSet = [];
+    this.index = 0;
+  }
+
+  add(value) {
+    for (let i = 0; this.hashSet[i]; i++) {
+      if (this.hashSet[i] === value) return;
+    }
+    this.hashSet[this.index] = value;
+    this.index++;
+  }
+
+  has(value) {
+    for (let i = 0; this.hashSet[i]; i++) {
+      if (this.hashSet[i] === value) return true;
+    }
+    return false;
+  }
+
+  delete(value) {
+    for (let i = 0; this.hashSet[i]; i++) {
+      if (this.hashSet[i] === value) {
+        this.hashSet.splice(i, 1);
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+// const main = () => {
+//   const hashMap = new HashMap();
+
+//   hashMap.set("a", 2);
+//   console.log(hashMap);
+//   // console.log(hashMap.get("a"));
+//   console.log(hashMap.has("a"));
+//   console.log(hashMap.delete("a"));
+//   console.log(hashMap.has("a"));
+//   console.log(hashMap);
+// };
+
+// main();
+
+export { HashMap, HashSet };
